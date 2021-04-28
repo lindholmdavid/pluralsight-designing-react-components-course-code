ab49b1d5420e82567be38693dc9d010c01366b58
lindholmdavid <david.4321@outlook.com>
2021-04-28 09:37:18 -0500

    refactor code to improve reusability in speaker.js

--- 02-designing-better-components/clip-030-components-to-code/src/components/Speakers/Speakers.js
+++ 02-designing-better-components/clip-030-components-to-code/src/components/Speakers/Speakers.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
-const Speakers = () => <img src="images/speakers.png" />;
+const Speakers = () => {
+    const speakers = [
+        {
+            imageSrc: "speakers",
+            name: 'All'
+        },
+    ];
 
+    return (
+        <div>
+            {speakers.map((speaker) => {
+                const { imageSrc, name } = speaker;
+                return <img src={`/images/${imageSrc}.png`}
+                    alt={name}
+                    key={imageSrc}></img>
+            })}
+        </div>
+    );
+};
+    
 export default Speakers;
